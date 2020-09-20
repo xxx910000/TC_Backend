b8d9b80d0af80c466fd882a66a595ab8b8567a18
xxx910000 <57673284+xxx910000@users.noreply.github.com>
2020-09-20 20:15:18 +0800

    9/20

--- public/javascripts/front-side.js
+++ public/javascripts/front-side.js
@@ -4,7 +4,7 @@ getGoods("food");
 getPvpData();
 getCourseData();
 
-function checkLogin(){
+function checkLogin() {
     if (!$.cookie('account') || $.cookie('account') == "null") {
         alert("請先登入會員");
         location.href = '../index.html';
@@ -13,10 +13,10 @@ function checkLogin(){
 }
 
 //取得user資料
-function getUserData(){
-    
-    $.post('/user',{ 'account': $.cookie('account')},function(res){
-        if(res.status==0){
+function getUserData() {
+
+    $.post('/user', { 'account': $.cookie('account') }, function (res) {
+        if (res.status == 0) {
             var username = $('#username').text(res.data.username);
             var score = $('#score').text(res.data.score);
             var money = $('#money').text(res.data.money);
@@ -24,7 +24,7 @@ function getUserData(){
 
             var a = res.data.goal[0].list
             var b = JSON.parse(a)
-            for(var i =0;i<b.length;i++){
+            for (var i = 0; i < b.length; i++) {
                 $('#week_').append(`<td>${b[i].week}</td>`)
                 $('#hour_').append(`<td>${b[i].hour}小時</td>`)
             }
@@ -33,86 +33,88 @@ function getUserData(){
 }
 
 //取得商品資料
-function getGoods(type){
-   
-    $.post('/shop',{'type':type},function(res){
-        if(res.status==0){
-            var i=1;
-            res.data.forEach(function(img){
+function getGoods(type) {
+
+    $.post('/shop', { 'type': type }, function (res) {
+        if (res.status == 0) {
+            var i = 1;
+            res.data.forEach(function (img) {
                 var goods_item = `
             <div class="goods-item">
                 <div class="goods-img"><img src="../image/shop/food/food_${i}.png" alt="food1"></div>
                 <div class="goods-button"><button>${img.price}</button></div>
             </div>`
 
-            $('#goods_').append(goods_item);
-            i++;
-            });   
+                $('#goods_').append(goods_item);
+                i++;
+            });
         }
     });
 }
 
 //取得戰績資料
-function getPvpData(){
-    var url = "/pvp?account="+$.cookie('account');
-    $.get(url,function(res){
-        if(res.status==0){
-            var rate = (res.data.winning/(res.data.winning+res.data.fail))*100;
-            $('#winning').text('勝利 '+res.data.winning);
-            $('#fail').text('失敗 '+res.data.fail);
-            $('#rate').text('勝率 '+rate+'%');
+function getPvpData() {
+    var url = "/pvp?account=" + $.cookie('account');
+    $.get(url, function (res) {
+        if (res.status == 0) {
+            var rate = (res.data.winning / (res.data.winning + res.data.fail)) * 100;
+            $('#winning').text('勝利 ' + res.data.winning);
+            $('#fail').text('失敗 ' + res.data.fail);
+            $('#rate').text('勝率 ' + rate + '%');
         }
     })
 }
 
 //儲存每日目標資料
-function saveGoalData(){
+function saveGoalData() {
     var week = document.getElementsByName('week');
     var hour = document.getElementsByName('hour');
     var goal = new Array();
-    for(var i =0;i<week.length;i++){
-        if(week[i].checked){
-            var goal_ ={week:week[i].value,
-                hour:hour[i].value};
-            goal.push(goal_); 
+    for (var i = 0; i < week.length; i++) {
+        if (week[i].checked) {
+            var goal_ = {
+                week: week[i].value,
+                hour: hour[i].value
+            };
+            goal.push(goal_);
         }
     }
 
-    var url = "/user/goal?account="+$.cookie('account');
+    var url = "/user/goal?account=" + $.cookie('account');
 
     $.ajax({
-        url:url,
-        type:"PATCH",
-        data:{list:JSON.stringify(goal)},
+        url: url,
+        type: "PATCH",
+        data: { list: JSON.stringify(goal) },
         //traditional:true,
         //processData:false,
         //contentType:"json/application",
         //dataType: "json",
-        success: function(res){
-            if(res.status==0){
+        success: function (res) {
+            if (res.status == 0) {
                 alert("設定成功！");
-                $('#login').css('display','none');
-                $('#fade').css('display','none');
+                $('#login').css('display', 'none');
+                $('#fade').css('display', 'none');
                 $('#week_').empty();
                 $('#hour_').empty();
                 getUserData();
             }
         },
-        error: function(err){
+        error: function (err) {
             console.log(err);
         }
     });
 }
 
 //取得課程資料
-function getCourseData(){
+function getCourseData() {
     var coursetype = "APP Inventor";
-    var url = "/course?coursetype="+coursetype;
-    $.get(url,function(res){
-        if(res.status==0){
-            res.data.forEach(function(course){
-                if(course.price == 0){
-                var course_item = `
+    var url = "/course?coursetype=" + coursetype;
+    $.get(url, function (res) {
+        if (res.status == 0) {
+            res.data.forEach(function (course) {
+                if (course.price == 0) {
+                    var course_item = `
                 <div class="left-item">
                     <div class="left-video">
                         <div class="left-img1"><img src="image/video2.png" alt="video2"></div>
@@ -124,8 +126,8 @@ function getCourseData(){
                     </div>
                 </div>`
 
-                $('#course_list').append(course_item);
-                }else{
+                    $('#course_list').append(course_item);
+                } else {
                     var course_item = `
                     <div class="left-item">
                     <div class="left-video">
@@ -137,8 +139,8 @@ function getCourseData(){
                         <div class="left-txt">${course.coursetype}<br>${course.coursechapter}<br><img src="image/money.png" alt="money"> ${course.price}</div>
                     </div>
                 </div>`
-                
-                $('#course_list').append(course_item);
+
+                    $('#course_list').append(course_item);
                 }
             })
         }
@@ -146,11 +148,11 @@ function getCourseData(){
 }
 
 //取得試題資料
-function getTestData(){
+function getTestData() {
     var url = "/test";
-    $.get(url,function(res){
-        if(res.status==0){
-            res.data.forEach(function(test){
+    $.get(url, function (res) {
+        if (res.status == 0) {
+            res.data.forEach(function (test) {
                 var test = `<div class="test">
                 <span class="question">題目 : ${test.題目}</span><br>
                 <input type="radio" name="${test.序號}" value="A">[A] ${test.選項A}<br>
@@ -158,16 +160,55 @@ function getTestData(){
                 <input type="radio" name="${test.序號}" value="C">[C] ${test.選項C}<br>
                 <input type="radio" name="${test.序號}" value="D">[D] ${test.選項D}<br>
             </div>`;
-            $('.right').append(test);
+                $('.right').append(test);
             })
         }
     })
 }
 
-function handAns(){
+function handAns() {
     var checked = $('input[type=radio]:checked');
-    for(var i=0;i<checked.length;i++){
-        console.log(checked[i].value)
-    }
- 
+    var record = new Array();
+    $.get("/test", function (res) {
+        if (res.status == 0) {
+            res.data.forEach(function (test) {
+                for (var i = 0; i < checked.length; i++) {
+                    if (test.序號 == checked[i].name) {
+                        var gain_ = (test.答案 == checked[i].value)? true:false;
+                        var record_ = {
+                            序號: checked[i].name,
+                            handAns: checked[i].value,
+                            答案: test.答案,
+                            gain: gain_,
+                        };
+                        record.push(record_);
+                        
+                    }
+                }
+            })
+            //var record_ = JSON.stringify(record);
+            //console.log(record_)
+            var url = "/test?account=" + $.cookie('account');
+            $.ajax({
+                url: url,
+                type: "PUT",
+                data: {list:JSON.stringify(record)},
+                //traditional:true,
+                //processData:false,
+                //contentType:"json/application",
+                //dataType: "json",
+                success: function (res) {
+                    if (res.status == 0) {
+                        var a = res.data.record[0].list;
+                        var b = JSON.parse(a)
+                        console.log(b)
+                        window.showModalDialog("test record.html",b);
+                    }
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+        }
+    })
 }
